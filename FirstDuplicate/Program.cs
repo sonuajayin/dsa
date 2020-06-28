15f6406bc3195016e8c4ffcc17905ddd0638a5af
Ajay <sonu.ajay.in@gmail.com>
2020-06-28 21:45:53 +0530

    Adding cs

---
+++ FirstDuplicate/Program.cs
@@ -0,0 +1,29 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[] { 1, 2, 3, 2, 4, 3, 5, 6, 7, 8, 5 };
+        HashSet<Int32> set = new HashSet<Int32>();
+
+        int firstDuplicate = -1;
+        int indexOfFirstDuplicate = -1;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int value = arr[i];
+            if (set.Add(value))
+            {
+                set.Add(value);
+            }
+            else
+            {
+                firstDuplicate = value;
+                indexOfFirstDuplicate = i;
+                break;
+            }
+        }
+
+        Console.WriteLine("First Duplicate on Index = " + indexOfFirstDuplicate + " Value = " + firstDuplicate);
+    }
+}
