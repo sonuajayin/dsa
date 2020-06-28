74ca2535187e3a82a606d119ad6388bdb1b669da
Ajay <sonu.ajay.in@gmail.com>
2020-06-28 21:24:32 +0530

    added java code to find first duplicate

---
+++ FirstDuplicate/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 3, 2, 4, 3, 5, 6, 7, 8, 5 };
+
+        Set<Integer> set = new HashSet<>();
+
+        int firstDuplicate = -1;
+        int indexOfFirstDuplicate = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            int value = arr[i];
+            if (set.add(value)) {
+                set.add(value);
+            } else {
+                firstDuplicate = value;
+                indexOfFirstDuplicate = i;
+                break;
+            }
+        }
+
+        System.out.println("First Duplicate on Index = " + indexOfFirstDuplicate + " Value = " + firstDuplicate);
+
+    }
+}
