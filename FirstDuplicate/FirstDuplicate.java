9eea99f41b09c5f9bd18e62d40918c031c5e3d07
Ajay <sonu.ajay.in@gmail.com>
2020-07-01 11:15:06 +0530

    Find Frequencies

--- FirstDuplicate/FirstDuplicate.java
+++ FirstDuplicate/FirstDuplicate.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class Main {
+public class FirstDuplicate {
     public static void main(String[] args) {
         int[] arr = new int[] { 1, 2, 3, 2, 4, 3, 5, 6, 7, 8, 5 };
 
